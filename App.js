f86c368fe1d39bc15ecfc6240f78557fcfe1d3bc
Montiralai <Montiralai.kraiwita@gmail.com>
2021-05-09 11:54:34 +0700

    montiralai

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+
+import Welcome from './screens/Welcome';
+
+import Bmi from './screens/Bmi';
+
+
+export default function App() {
+  return (
+    
+    <Bmi />
+
+  );
+}
+
+
+
---
+++ screens/Ex11.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Ex11() {
+  return (
+    <View style={{flex:2 }}>    
+      <View style={{ backgroundColor : '#4A90E2' , flex : 1 }}></View>
+       <View style={{ backgroundColor : '#50E3C2' ,  flex : 1 }}></View>
+            
+    </View>
+  );
+}
